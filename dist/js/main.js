2495a6215892ca179dd2ec05ed5bd25ea9f883c8
Kathleen Yeh <kathleenyeh1@gmail.com>
2020-11-07 02:08:19 -0800

    header finished

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,10 +1,13 @@
-const header = document.querySelector('main-header');
+const header = document.querySelector('.main-header');
+const logoImg = document.querySelector('.logoImg');
 
 window.addEventListener('scroll', () => {
-  const scrollPosition = window.scrollY;
+  const scrollPos = window.scrollY;
   if (scrollPos > 10) {
     header.classList.add('scrolled');
+    logoImg.src = 'dist/img/logo-white.PNG';
   } else {
     header.classList.remove('scrolled');
+    logoImg.src = 'dist/img/logo-plain.PNG';
   };
 });
